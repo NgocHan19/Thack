071f7fe10f0a7928236124aa70ff9184fd2da82d
BachMieuuu <tranngochan2004.vn@gmail.com>
2024-10-26 14:19:00 +0700

    THACK-21 <Trang sua thong tin ca nhan>

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import Menu_NV from './component/Menu/Menu_NV';
 import Menu_QL from './component/Menu/Menu_QL';
 import Logout from './component/Account/Logout';
 import HomePage from './component/HomePage/HomePage';
+import UserProfile from './component/Account/UserProfile';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<HomePage />} />
+    //     <Route path="/login" element={<Login />} />
+    //   </Routes>
+    // </Router>
+    <UserProfile/>
   );
 }
 
